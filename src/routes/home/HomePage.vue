da7d9d1c55b1d963d1b525250f44a5abf726c1aa
Max <hello@max.berlin>
2020-03-31 15:00:30 +0200

    Fix: name conventions

--- src/routes/home/HomePage.vue
+++ src/routes/home/HomePage.vue
@@ -1,13 +1,13 @@
 <template>
     <div>
-        <h1>Hi {{account.user.firstName}}!</h1>
+        <h1>Hi {{account.user.firstname}}!</h1>
         <p>You're logged in with Vue + Vuex & JWT!!</p>
         <h3>Users from secure api end point:</h3>
         <em v-if="users.loading">Loading users...</em>
         <span v-if="users.error" class="text-danger">ERROR: {{users.error}}</span>
         <ul v-if="users.items">
             <li v-for="user in users.items" :key="user.id">
-                {{user.firstName + ' ' + user.lastName}}
+                {{user.firstname + ' ' + user.lastname}}
                 <span v-if="user.deleting"><em> - Deleting...</em></span>
                 <span v-else-if="user.deleteError" class="text-danger"> - ERROR: {{user.deleteError}}</span>
                 <span v-else> - <a @click="deleteUser(user.id)" class="text-danger">Delete</a></span>
--- src/routes/register/RegisterPage.vue
+++ src/routes/register/RegisterPage.vue
@@ -6,23 +6,23 @@
               <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
                 First Name
               </label>
-              <input v-model="user.firstName" required name="firstName" class="appearance-none block w-full bg-gray-200 text-gray-700 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane">
+              <input v-model="user.firstname" required name="firstname" class="appearance-none block w-full bg-gray-200 text-gray-700 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane">
 
             </div>
             <div class="w-full md:w-1/2 px-3">
               <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
                 Last Name
               </label>
-              <input v-model="user.lastName" required name="lastName" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe">
+              <input v-model="user.lastname" required name="lastname" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe">
 
             </div>
           </div>
           <div class="flex flex-wrap -mx-3 mb-6">
             <div class="w-full px-3">
-              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-username">
-                Username
+              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-email">
+                email
               </label>
-              <input v-model="user.username" required name="username" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-username" type="text" placeholder="some username">
+              <input v-model="user.email" required name="email" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-email" type="email" placeholder="Your email">
             </div>
           </div>
           <div class="flex flex-wrap -mx-3 mb-6">
@@ -50,9 +50,9 @@ export default {
     data () {
         return {
             user: {
-                firstName: '',
-                lastName: '',
-                username: '',
+                firstname: '',
+                lastname: '',
+                email: '',
                 password: ''
             },
             submitted: false
